6283eafa7ff119c4df4e0ae390a0d8e7a0e48858
Joshua Fluke <Jfluke717@gmail.com>
2016-01-17 17:52:52 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Setup
+
+run `npm install`
+
+then run `npm run serve` to serve the files on a local dev server.
+
+## Testing
+
+To log out of your twitter account, clear your local storage for the hosted domain.
---
+++ js/app.js
@@ -0,0 +1,164 @@
+'use strict';
+
+var app = angular.module('tweeterApp', []);
+
+app.directive('jfMainApp', function () {
+  return {
+    restrict: 'EA',
+    templateUrl: 'partials/jfMainApp.html',
+    scope: {},
+    controllerAs: 'jfMainCtrl',
+    controller: function (twitterService) {
+      var self = this;
+      twitterService.initialize();
+      this.authenticated = twitterService.isReady();
+      this.successfulSubmissions = [];
+      this.failedSubmissions = [];
+
+      this.authenticate = function () {
+        twitterService.connectTwitter().then(function() {
+          self.authenticated = twitterService.isReady();
+        });
+      };
+
+      this.tweet = function (tweetMsg) {
+        if (tweetMsg === undefined) {
+          tweetMsg = '';
+        }
+
+        this.isValidTweet(tweetMsg, function (err) {
+          if (err) {
+            alert(err);
+            self.failedSubmissions.push({
+              tweet: tweetMsg
+            });
+          } else {
+            twitterService.postTweet(tweetMsg)
+              .then(function (tweet) {
+                self.successfulSubmissions.push({
+                  id_str: tweet.id_str,
+                  tweet:  tweet.text
+                });
+              });
+          }
+        });
+      };
+
+      this.isValidTweet = function (tweet, callback) {
+        var containsMicrosoft;
+        var containsWindows;
+        var containsAWS;
+
+        if (tweet.indexOf('Microsoft') !== -1) {
+          containsMicrosoft = true;
+        }
+        if (tweet.indexOf('Windows') !== -1) {
+          containsWindows = true;
+        }
+        if (tweet.indexOf('AWS') !== -1) {
+          containsAWS = true;
+        }
+
+        if (tweet.length > 140) {
+          callback('Tweet more than 140 characters');
+        }
+
+        if (tweet.length < 5) {
+          callback('Tweet less than 5 characters');
+        }
+
+        if (containsAWS) {
+          // valid
+          callback();
+        }
+
+        if (containsMicrosoft || containsWindows) {
+          callback('Contains Microsoft or Windows');
+        }
+
+        // valid
+        callback();
+      };
+
+    },
+    link: function (scope, element, attrs, ctrl) {
+    }
+  };
+});
+
+app.service('twitterService', function ($http, $q) {
+  var applicationId = '40R_N812UhrnKamvlzBTein12hs';
+  var authorizedTwitter = false;
+
+  this.initialize = function () {
+    OAuth.initialize(applicationId, {
+      cache: true
+    });
+    authorizedTwitter = OAuth.create('twitter');
+  };
+
+  this.isReady = function () {
+    return (authorizedTwitter);
+  };
+
+  this.connectTwitter = function () {
+    var deferred = $q.defer();
+    OAuth.popup('twitter', {
+      cache: true
+    }, function (error, result) {
+      if (error) {
+        alert('There was a problem connecting to twitter');
+      } else {
+        authorizedTwitter = result;
+        deferred.resolve();
+      }
+    });
+    return deferred.promise;
+  };
+
+  this.postTweet = function (newTweet) {
+    var deferred = $q.defer();
+    var url = '/1.1/statuses/update.json';
+
+    authorizedTwitter.post(url, {
+      data: {
+        status: newTweet
+      }
+    })
+      .done(function (data) {
+        deferred.resolve(data);
+      })
+      .fail(function (err) {
+        deferred.reject(err);
+      });
+
+    return deferred.promise;
+  };
+
+  this.clearCache = function () {
+    OAuth.clearCache('twitter');
+    authorizedTwitter = false;
+  };
+
+});
+
+app.directive('jfTwitterSubmissions', function () {
+  return {
+    restrict: 'EA',
+    template: '<div ng-repeat="item in submissions">' +
+              '<div class="submission_id" ng-show="item.id_str">Submission id_str: {{ item.id_str }}</div>' +
+              '<div class="submission_tweet">Submission text: {{ item.tweet }}</div>' +
+              '</div>',
+    scope: {
+      submissions: '='
+    },
+    link: function (scope, element, attrs, ctrl) {
+    }
+  };
+});
+
+// to-do
+// need to host this, possibly on heroku
+
+// (things that could be done to improve this)
+// more scalable structure, tests, minify source code
